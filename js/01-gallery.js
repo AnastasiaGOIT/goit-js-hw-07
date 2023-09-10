ae9c9827813a65cb512d7b053b9e28c3e82d5a69
AnastasiaGOIT <hudzevych.music@gmail.com>
2023-09-10 11:36:38 +0300

    hm7-try

---
+++ js/01-gallery.js
@@ -0,0 +1,61 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+
+const list = document.querySelector(".gallery", createMarkUp);
+list.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
+list.addEventListener("click", onClick);
+
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+   data-source="${original}" 
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target === evt.currentTarget) {
+    return;
+  }
+  const currentImg = evt.target.closest(".gallery__item");
+  const fullImg = evt.target.dataset.source;
+
+  function onKey(evt) {
+    if (!evt.code === "Esc") {
+      return;
+    }
+    instance.close();
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img
+      class="gallery__image"
+      src="${fullImg}"
+    />
+`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", onKey);
+      },
+    },
+    {
+      onClose: (instance) => {
+        document.removeEventListener();
+      },
+    }
+  );
+
+  instance.show();
+}
