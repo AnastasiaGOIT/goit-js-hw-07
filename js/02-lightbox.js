150e492e7d0d4c23d1a126b278623b731eab4b29
AnastasiaGOIT <hudzevych.music@gmail.com>
2023-09-11 21:43:57 +0300

    js-8-edit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,8 +2,6 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const list = document.querySelector(".gallery", createMarkUp);
 list.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
-list.addEventListener("click", onClick);
-// const url = galleryItems.original;
 
 function createMarkUp(arr) {
   return arr
@@ -16,15 +14,8 @@ function createMarkUp(arr) {
     )
     .join("");
 }
-function onClick(evt) {
-  evt.preventDefault();
-  if (!evt.target === evt.currentTarget) {
-    return;
-  }
-  const currentImg = evt.target.closest(".gallery__item");
-}
+
 var lightbox = new SimpleLightbox(".gallery__item a", {
   captionsData: "alt",
   captionDelay: "250",
 });
-console.log(galleryItems);
